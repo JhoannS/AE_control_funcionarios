25ac25c17a155de482ce73ab523250cdc039f265
JhoannS <sebastianzamudio2004@gmail.com>
2024-03-07 12:39:13 -0500

    ADD PORT

--- src/keys.js
+++ src/keys.js
@@ -3,8 +3,7 @@ module.exports = {
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
-        port: process.env.PORT || 3307,
-        portSQL: process.env.PORT_SQL,
+        port: process.env.DB_PORT || 3307, 
         database: process.env.DB_NAME || 'Ae'
     }
 }
