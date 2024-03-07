f0cc531ffc39b20e949b8edfff9aa55726ec8675
JhoannS <sebastianzamudio2004@gmail.com>
2024-03-07 13:21:34 -0500

    arreglo de subir aarchivo

--- src/routes/indexFun.js
+++ src/routes/indexFun.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const pool = require('../database');
 const fs = require('fs');
-const multer = require("multer");
 const { estaLogueado } = require("../lib/auth");
 
 // ::::::::::::::::::::::::::::::::::::::::::
