d62fd2df9cbca59214baf5431ec0929a375b2653
JhoannS <sebastianzamudio2004@gmail.com>
2024-03-07 11:01:12 -0500

    actualizacion del puerto

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
-        port: 12209 || 3307, 
+        port: process.env.DB_PORT || 3307, 
         database: process.env.DB_NAME || 'Ae'
     }
 }
